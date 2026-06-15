7e8c2dca73df3594911d80df71fbebfc57971777
dong9191 <rla50601123@gmail.com>
2026-06-15 15:50:02 +0900

    unity

---
+++ chapter 7/Assets/bamsongigenerater.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class BamsongiGenerator : MonoBehaviour
+{
+    public GameObject bamsongiPrefab;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bamsongi = Instantiate(bamsongiPrefab);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            bamsongi.GetComponent<BamsongiController>().Shoot(ray.direction * 2000);
+        }
+    }
+}
